732ff489d323251ff3bdd14bd370383d02b6fdae
yr2672-blip <yr2672@srmist.edu.in>
2026-03-02 15:16:50 +0530

    yuvraj added UC1

---
+++ APP/src/ParliandromeCheckerapp.java
@@ -0,0 +1,14 @@
+
+public class ParliandromeCheckerapp {
+
+    static final String APP_NAME = "Parliandrome Checker App";
+    static final String VERSION = "v1.0";
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to " + APP_NAME);
+        System.out.println("Version: " + VERSION);
+        System.out.println("=================================");
+    }
+}
